14de6eaccfcb0c886c43380207a4749d395322d7
FirstREQUIEM <147359864+FirstREQUIEM@users.noreply.github.com>
2024-07-04 21:49:07 +0700

    add php pages

---
+++ stylerambut.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LE Barbershop Point</title>
+    <link rel="stylesheet" href="cssindex.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<script type="text/javascript" src="fungsijava.js"></script>
+</head>
+<body>
+    <div class="banner">
+        <div class="navbar">
+            <img class="logo" src="pictures/LE_BARBERSHOP_POINT.logo.png ">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="stylerambut.php">Style</a></li>
+                <li><a href="displayantre.html">Queue</a></li>
+            </ul>
+        </div>
+        <div class="new-visitor" style="width: 1500px; height: 500px; padding-top: 28px;">
+        <div class="rambut-model">
+            <img style="width: 200px;" class="model" src="pictures/Model_rambut.png">
+            <h3 class="Model-rambut">Model Akabri</h3><br>
+            <p class="Model-rambut-text">model rambut yang digunakan oleh para taruna akmil, akpol, aau, aal, maupun taruna kedinasan </p>
+        </div>
+        <div class="rambut-model">
+            <img style="width: 200px;" class="model" src="pictures/Model_rambut_cepak.png">
+            <h3 class="Model-rambut">Model Cepak</h3><br>
+            <p class="Model-rambut-text">model rambut yang digunakan oleh para TNI   </p>
+        </div>
+        <div class="rambut-model">
+            <img style="width: 200px;" class="model" src="pictures/Model_rambut _plontos.png">
+            <h3 class="Model-rambut">Model plontos</h3><br>
+            <p class="Model-rambut-text">model rambut simpel</p>
+        </div>
+        <div class="rambut-model">
+            <img style="width: 200px;" class="model" src="pictures/Model_rambut _korea.png">
+            <h3 class="Model-rambut">Model Korea</h3><br>
+            <p class="Model-rambut-text">model rambut yang digunakan oleh artis kpop korea </p>
+        </div>
+    </div>
+
+    <div class="footer-bawah">
+        <p>Dikerjaan oleh 077<p>
+    </div>
+</body>
+</html>
